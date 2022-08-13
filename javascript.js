3a6d07bfb6cde6b9a1dd23f8d5003bac03b3e41b
Rahul Sharma <rahul.sharma0001@hotmail.com>
2022-08-13 16:13:19 -0400

    Ask player to pick Rock, Paper, or Scissors

--- javascript.js
+++ javascript.js
@@ -5,3 +5,8 @@ function getComputerChoice() {
     let computerChoice =  choices[Math.floor(Math.random() * choices.length)];
     return computerChoice;
 }
+
+function getPlayerChoice() {
+    let playerChoice = prompt("What do you pick: Rock, Papers, or Scissors:");
+    return playerChoice;
+}
