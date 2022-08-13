4a64c4fbc037818158b9499e174439da6050ce4f
Rahul Sharma <rahul.sharma0001@hotmail.com>
2022-08-13 18:21:51 -0400

    Add function to play a single round and declare a winner for that round

--- javascript.js
+++ javascript.js
@@ -12,3 +12,50 @@ function getPlayerChoice() {
     return playerChoice;
 }
 
+const computerSelection = getComputerChoice();
+//console.log(computerSelection);//
+
+const playerSelection = getPlayerChoice();
+//console.log(playerSelection);//
+
+function play() {
+
+    let result;
+
+    if (computerSelection === playerSelection) {
+        result = console.log("It's a tie!");
+    }
+
+    else if (computerSelection === 'rock' && playerSelection === 'scissors') {
+        result = "The computer scored a point. Rock beats scissors.";
+    }
+    
+    else if (computerSelection === 'rock' && playerSelection === 'paper') {
+        result = "You scored a point. Paper beats rock.";
+    }
+
+    else if (computerSelection === 'paper' && playerSelection === 'rock') {
+        result = "The computer scored a point. Paper beats rock.";
+    }
+
+    else if (computerSelection === 'paper' && playerSelection === 'scissors') {
+        result = "You scored a point. Scissors beats paper.";
+    }
+
+    else if (computerSelection === 'scissors' && playerSelection === 'paper') {
+        result = "The computer scored a point. Scissors beats paper.";
+    }
+
+    else if (computerSelection === 'scissors' && playerSelection === 'rock') {
+        result = "You scored a point. Rock beats scissors.";
+    }
+
+    else {
+        result = "You can only pick one from: rock, paper, or scissors. Please try again."
+    }
+
+    return result;
+}
+
+const outcome = play();
+console.log(outcome);
