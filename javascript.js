14066696446f329a326fd276109664d3899835b8
Rahul Sharma <rahul.sharma0001@hotmail.com>
2022-08-13 20:52:31 -0400

    Add game function and change total number of rounds to 5

--- javascript.js
+++ javascript.js
@@ -7,55 +7,49 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-    let playerChoice = prompt("What do you pick: Rock, Papers, or Scissors:");
+    let playerChoice = prompt("What do you pick: Rock, Paper, or Scissors:");
     playerChoice = playerChoice.toLowerCase();
     return playerChoice;
 }
 
-const computerSelection = getComputerChoice();
-//console.log(computerSelection);//
-
-const playerSelection = getPlayerChoice();
-//console.log(playerSelection);//
-
 function play() {
 
-    let result;
+    const computerSelection = getComputerChoice();
+    const playerSelection = getPlayerChoice();
 
     if (computerSelection === playerSelection) {
         result = console.log("It's a tie!");
     }
-
     else if (computerSelection === 'rock' && playerSelection === 'scissors') {
         result = "The computer scored a point. Rock beats scissors.";
     }
-    
     else if (computerSelection === 'rock' && playerSelection === 'paper') {
         result = "You scored a point. Paper beats rock.";
     }
-
     else if (computerSelection === 'paper' && playerSelection === 'rock') {
         result = "The computer scored a point. Paper beats rock.";
     }
-
     else if (computerSelection === 'paper' && playerSelection === 'scissors') {
         result = "You scored a point. Scissors beats paper.";
     }
-
     else if (computerSelection === 'scissors' && playerSelection === 'paper') {
         result = "The computer scored a point. Scissors beats paper.";
     }
-
     else if (computerSelection === 'scissors' && playerSelection === 'rock') {
         result = "You scored a point. Rock beats scissors.";
     }
-
     else {
         result = "You can only pick one from: rock, paper, or scissors. Please try again."
     }
-
     return result;
 }
 
-const outcome = play();
-console.log(outcome);
+function game() {
+
+    for(i = 0; i < 5; i++) {
+    const output = play();
+    console.log(output);
+    }
+}
+
+game()
